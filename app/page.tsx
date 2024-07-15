7a515c93e2c99be57833c4eabffaed500d954bbe
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-15 19:26:09 +0530

    modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Signup from "./components/SigninComponent";
 
 export default function Home() {
   return (
     <div>
-      Hi there
+      <Signup />
     </div>
   );
 }
